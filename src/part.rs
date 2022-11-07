852c5e7b1934590c9492c4dffba45c5bb498e2d5
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2022-11-07 16:46:44 +0100

    Fix timestep drift velocity computation

--- src/part.rs
+++ src/part.rs
@@ -1,5 +1,9 @@
 pub use crate::physical_quantities::{Conserved, Primitives};
-use crate::{engine::{Engine, ParticleMotion}, equation_of_state::EquationOfState, timeline::*};
+use crate::{
+    engine::{Engine, ParticleMotion},
+    equation_of_state::EquationOfState,
+    timeline::*,
+};
 
 #[derive(Default, Debug, Clone)]
 pub struct Part {
@@ -21,39 +25,55 @@ pub struct Part {
 }
 
 impl Part {
-    pub fn timestep(&mut self, cfl_criterion: f64, eos: &EquationOfState, particle_motion: &ParticleMotion) -> f64 {
-        // Set the velocity with which this particle will be drifted over the course of it's next timestep
-        let fluid_v = self.conserved.momentum() / self.conserved.mass();
-        if self.conserved.mass() > 0. {
-            self.v = match particle_motion {
-                ParticleMotion::FIXED => 0.,
-                ParticleMotion::FLUID => fluid_v,
-                ParticleMotion::STEER => {
-                    let d = self.centroid - self.x;
-                    let abs_d = d.abs();
-                    let r = 0.5 * self.volume;
-                    let eta = 0.25;
-                    let eta_r = eta * r;
-                    let xi = 1.0;
-                    let sound_speed = eos.sound_speed(self.primitives.pressure(), 1. / self.primitives.density());
-                    if abs_d > 0.9 * eta_r {
-                        let mut fac = xi * sound_speed / abs_d;
-                        if abs_d < 1.1 * eta_r {
-                            fac *= 5. * (abs_d - 0.9 * eta_r) / eta_r;
-                        }
-                        fluid_v + fac * d
-                    } else {
-                        fluid_v
-                    }
-                }
-            };
-        } else {
+    pub fn timestep(
+        &mut self,
+        cfl_criterion: f64,
+        eos: &EquationOfState,
+        particle_motion: &ParticleMotion,
+    ) -> f64 {
+        if self.conserved.mass() == 0. {
+            // We have vacuum
+            debug_assert_eq!(self.conserved.momentum(), 0.);
+            debug_assert_eq!(self.conserved.energy(), 0.);
             self.v = 0.;
+            return f64::INFINITY;
         }
+
+        // Normal case
+        debug_assert!(self.conserved.mass() > 0.);
+
+        let mass_inv = 1. / self.conserved.mass();
+        let fluid_v = self.conserved.momentum() * mass_inv;
+        let sound_speed = eos.sound_speed(
+            eos.gas_pressure_from_energy(self.conserved.energy(), self.volume),
+            self.volume * mass_inv,
+        );
+        // Set the velocity with which this particle will be drifted over the course of it's next timestep
+        self.v = match particle_motion {
+            ParticleMotion::FIXED => 0.,
+            ParticleMotion::FLUID => fluid_v,
+            ParticleMotion::STEER => {
+                let d = self.centroid - self.x;
+                let abs_d = d.abs();
+                let r = 0.5 * self.volume;
+                let eta = 0.25;
+                let eta_r = eta * r;
+                let xi = 1.0;
+                if abs_d > 0.9 * eta_r {
+                    let mut fac = xi * sound_speed / abs_d;
+                    if abs_d < 1.1 * eta_r {
+                        fac *= 5. * (abs_d - 0.9 * eta_r) / eta_r;
+                    }
+                    fluid_v + fac * d
+                } else {
+                    fluid_v
+                }
+            }
+        };
 
         // determine the size of this particle's next timestep
         let v_rel = (self.v - fluid_v).abs();
-        let v_max = v_rel + eos.sound_speed(self.primitives.pressure(), 1. / self.primitives.density());
+        let v_max = v_rel + sound_speed;
 
         if v_max > 0. {
             0.5 * cfl_criterion * self.volume / v_max
@@ -79,10 +99,10 @@ impl Part {
                 let p = self.primitives.pressure();
                 self.extrapolations -= dt_extrapolate
                     * Primitives::new(
-                    rho * self.gradients.velocity() + v * self.gradients.density(),
-                    v * self.gradients.velocity() + rho_inv * self.gradients.pressure(),
-                    gamma * p * self.gradients.velocity() + v * self.gradients.pressure(),
-                )
+                        rho * self.gradients.velocity() + v * self.gradients.density(),
+                        v * self.gradients.velocity() + rho_inv * self.gradients.pressure(),
+                        gamma * p * self.gradients.velocity() + v * self.gradients.pressure(),
+                    )
             }
         } else {
             unimplemented!()
@@ -124,7 +144,11 @@ impl Part {
         };
 
         if self.primitives.density() < 1e-6 {
-            self.primitives = Primitives::new(self.primitives.density(), self.primitives.velocity() * self.primitives.density() * 1e6, self.primitives.pressure());
+            self.primitives = Primitives::new(
+                self.primitives.density(),
+                self.primitives.velocity() * self.primitives.density() * 1e6,
+                self.primitives.pressure(),
+            );
             self.conserved = Conserved::from_primitives(&self.primitives, self.volume, eos);
         }
 
