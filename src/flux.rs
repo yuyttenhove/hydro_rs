5048e2e9778f063370b4b3450b113084c3884466
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-02-13 15:58:58 +0100

    cleanup flux exchange

--- src/flux.rs
+++ src/flux.rs
@@ -26,7 +26,7 @@ pub fn flux_exchange(
     let shift = face.shift().unwrap_or(DVec3::ZERO);
     let dx_right = face.centroid() - right.centroid - shift;
     let dx_centroid = right.centroid + shift - left.centroid;
-    let dx = right.x + shift  - left.x;
+    let dx = right.x + shift - left.x;
 
     // Calculate the maximal signal velocity (used for timestep computation)
     let mut v_max = 0.;
@@ -53,23 +53,22 @@ pub fn flux_exchange(
     );
 
     // Compute interface velocity (Springel (2010), eq. 33):
-    let midpoint = 0.5 * (left.x + right.x);
+    let midpoint = 0.5 * (left.x + right.x + shift);
     let fac = (right.v - left.v).dot(face.centroid() - midpoint) / dx.length_squared();
     let v_face = 0.5 * (left.v + right.v) - fac * dx;
 
     // Calculate fluxes
-    let fluxes = face.area()
-        * engine.solver.solve_for_flux(
-            &primitives_left.boost(-v_face),
-            &primitives_right.boost(-v_face),
-            v_face,
-            face.normal(),
-            eos,
-        );
+    let fluxes = face.area() * engine.solver.solve_for_flux(
+        &primitives_left.boost(-v_face),
+        &primitives_right.boost(-v_face),
+        v_face,
+        face.normal(),
+        eos,
+    );
 
     FluxInfo {
         fluxes: dt * fluxes,
-        mflux: dx_centroid * fluxes.mass(),
+        mflux: dx * fluxes.mass(),
         v_max,
     }
 }
@@ -94,58 +93,51 @@ pub fn flux_exchange_boundary(
     }
 
     // Gradient extrapolation
-    let primitives_dash =
+    let mut primitives_dash =
         part.primitives + part.gradients.dot(dx_face).into() + part.extrapolations;
 
-    let fluxes = face.area()
-        * match boundary {
-            Boundary::Reflective => {
-                // Also reflect velocity
-                reflected = reflected.reflect_quantities(face.normal());
+    let primitives_boundary = match boundary {
+        Boundary::Reflective => {
+            // Also reflect velocity
+            reflected = reflected.reflect_quantities(face.normal());
 
-                v_max -= (reflected.primitives.velocity() - part.primitives.velocity())
-                    .dot(dx_centroid)
-                    .min(0.);
+            // Apply pairwise limiter
+            primitives_dash =
+                pairwise_limiter(&part.primitives, &reflected.primitives, &primitives_dash);
+            v_max -= (reflected.primitives.velocity() - part.primitives.velocity())
+                .dot(dx_centroid)
+                .min(0.);
 
-                let primitives =
-                    pairwise_limiter(&part.primitives, &reflected.primitives, &primitives_dash);
+            primitives_dash.reflect(face.normal())
+        }
+        Boundary::Open => {
+            primitives_dash =
+                pairwise_limiter(&part.primitives, &part.primitives, &primitives_dash);
 
-                // get reflected primitives at face
-                let primitives_reflected = primitives.reflect(face.normal());
+            primitives_dash
+        }
+        Boundary::Vacuum => {
+            let vacuum = Primitives::vacuum();
+            primitives_dash = pairwise_limiter(&part.primitives, &vacuum, &primitives_dash);
+            v_max += part.primitives.velocity()
+                .dot(dx_centroid)
+                .min(0.);
 
-                engine.solver.solve_for_flux(
-                    &primitives,
-                    &primitives_reflected,
-                    DVec3::ZERO,
-                    face.normal(),
-                    eos,
-                )
-            }
-            Boundary::Open => {
-                let primitives =
-                    pairwise_limiter(&part.primitives, &part.primitives, &primitives_dash);
+            vacuum
+        }
+        Boundary::Periodic => {
+            unreachable!("This function should not be called with periodic boundary conditions!");
+        }
+    };
 
-                engine.solver.solve_for_flux(
-                    &primitives,
-                    &primitives,
-                    DVec3::ZERO,
-                    face.normal(),
-                    eos,
-                )
-            }
-            Boundary::Vacuum => {
-                let vacuum = Primitives::vacuum();
-
-                let primitives = pairwise_limiter(&part.primitives, &vacuum, &primitives_dash);
-
-                engine
-                    .solver
-                    .solve_for_flux(&primitives, &vacuum, DVec3::ZERO, face.normal(), eos)
-            }
-            Boundary::Periodic => {
-                unreachable!("This method should not be called with periodic boundary conditions!");
-            }
-        };
+    // Solve for flux
+    let fluxes = face.area() * engine.solver.solve_for_flux(
+        &primitives_dash,
+        &primitives_boundary,
+        DVec3::ZERO,
+        face.normal(),
+        eos,
+    );
 
     FluxInfo {
         fluxes: part.dt * fluxes,
