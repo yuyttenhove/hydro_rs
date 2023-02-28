647209999774804ee49b8ee7cbaa9f909f92bf36
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-02-28 14:21:25 +0100

    testing

--- python/plot_slice.py
+++ python/plot_slice.py
@@ -9,7 +9,7 @@ def plot_slice(fname, savename):
     coords = np.column_stack([data["x"].values, data["y"].values, data["z"].values])
     values = data["rho"]
     x_lim = [0., 1.]
-    y_lim = [0., 1.5]
+    y_lim = [0., 1.]
     res = 1500
 
     interpolated = get_slice(values, coords, x_lim, y_lim, res)
@@ -21,6 +21,6 @@ def plot_slice(fname, savename):
 
 if __name__ == "__main__":
     from utils import get_root
-    n = 150
-    plot_slice(get_root() / "run" / "output" / f"rayleigh_taylor_2D_{n:04}.txt", get_root() / "run/slice_rt.png")
+    n = 6
+    plot_slice(get_root() / "run" / "output" / f"KelvinHelmholtz_HLLC_boosted_2D_{n:04}.txt", get_root() / "run/slice_hllc.png")
     
