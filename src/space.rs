1989e5af6931e206f0927f8f76cb71d87868b59d
Yolan Uyttenhove <yolan.uyttenhove@ugent.be>
2023-04-07 11:27:38 +0200

    small change

--- src/space.rs
+++ src/space.rs
@@ -54,18 +54,19 @@ macro_rules! get_other {
     };
 }
 
+
+macro_rules! create_attr {
+    ($group:expr, $data:expr, $name:expr) => {
+        $group.new_attr_builder().with_data(&$data).create($name)
+    };
+}
+
 macro_rules! create_dataset {
     ($group:expr, $data_iter:expr, $name:expr) => {
         $group
             .new_dataset_builder()
             .with_data(&$data_iter.collect::<Vec<_>>())
             .create($name)
-    };
-}
-
-macro_rules! create_attr {
-    ($group:expr, $data:expr, $name:expr) => {
-        $group.new_attr_builder().with_data(&$data).create($name)
     };
 }
 
